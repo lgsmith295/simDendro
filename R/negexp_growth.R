a2ad8f7327a3c6eb1db5d387e5e942266f1810b4
djhocking <djhocking@gmail.com>
2019-03-03 00:03:26 -0500

    started vignette

--- R/negexp_growth.R
+++ R/negexp_growth.R
@@ -1,5 +1,23 @@
-#####
+#' Add together two numbers.
+#'
+#' @param n Number of years to be simulated
+#' @param a Intercept
+#' @param b Rate of declining growth with age
+#' @param k Offset for minimum ring growth. Defaults to 0
+#' @return vector of expected ring widths due to biological growth. length \code{n}.
+#' @examples
+#' growth <- negexp_growth(50, 1, 0.1, 0.0001)
+#' plot(growth, type = "b")
 
-negex_growth <- function(n) {
-
+negexp_growth <- function(n, a, b, k = 0) {
+  if(n < 1) stop("n must be a positive integer")
+  t <- 1:n
+  rwl_bio <- a * exp(-b * t) + k
+  return(rwl_bio)
 }
+
+growth <- negexp_growth(50, 1, 0.1, 0.0001)
+plot(growth, type = "b")
+
+# test
+negex_growth(0, 1, 0.1)
